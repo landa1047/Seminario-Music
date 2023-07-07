69b331d6acb0686ac2be0d837b8afebf8f1b40c4
andres <andresh.g@hotmail.es>
2023-07-06 21:01:38 -0500

    carga git 2

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule , CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
@@ -8,7 +8,10 @@ import { HomePageRoutingModule } from './home-routing.module';
 
 
 @NgModule({
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  
   imports: [
+    
     CommonModule,
     FormsModule,
     IonicModule,
